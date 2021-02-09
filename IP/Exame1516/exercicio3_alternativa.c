e37e44332d1c06e9ce35dd16acc69c56a75be84c
Diogo <dlsequeira75@gmail.com>
2021-02-09 13:50:24 +0000

    Changed: Ex3 Alt do Exame Normal 1516

--- IP/Exame1516/exercicio3_alternativa.c
+++ IP/Exame1516/exercicio3_alternativa.c
@@ -14,18 +14,32 @@ void main()
 	if (func(f1, f2, final, TAMFIM))
 		printf("Frase modificada : \n % s\n", final);
 	else
-		printf("Frase n�o modificada!\n");
+		printf("Frase nao modificada!\n");
 }
 
 /*Resolução do exercicio 3*/
 
 int func(char st1[], char st2[], char stfim[], int tamf)
 {
+
+	/*
+		o objetivo e copiar o conteudo das strings st1 e st2 para a stfim, alternando os carateres.
+
+		a estrategia foi a seguinte:
+
+		os elementos pares da stfim vao ser preenchidos com os conteudos da st1 e os impares pela st2, até que a string mais pequena acabe,
+		e a partir dai o programa preenche o resto
+
+		a funcao vai usar um ciclo for para percorrer as strings, onde o i serve para percorrer os elementos da stfim e o k para as do st1 e st2.
+
+		i vai ser sempre incrementado em todas as iterações do ciclo, mas o k só quando verificar o conteudo do elemento k da st1 e st2
+
+	*/
+
+
 	int i = 0, k = 0; /*variáveis para controlo dos ciclos for*/
 
-	int variavel_de_apoio = 0;
-
-	if (strlen(st1) == 0 && strlen(st2) == 0) /*se as duas strings tiverem vazias, devolve 0*/
+	if (strlen(st1) == 0 && strlen(st2) == 0 || (strlen(st1) + strlen(st2)) > tamf) /*se as duas strings tiverem vazias ou se o tamanho de ambas as frases ultrapassar o tamanho da ultima, devolve 0*/
 		return 0;
 	/*o resto do código podia ser posto dentro de um else, mas é redudante por que a função termina se a condição do primeiro if for verdadeira*/
 
